5cc56a8f5485f4a1865ddf61a42f76c4060a2634
Syed01010100 <syedaseeka@hotmail.com>
2024-07-09 17:55:42 +0300

    Add files via upload

---
+++ Count_word.py
@@ -0,0 +1,4 @@
+Given_Sentence = "Guvi Geeks Network Privat Limited"
+split_list = Given_Sentence.split()
+"""Split convert each and every word present in a strang as an eleemnt to a list so if we give a input as a sentence it will convert the sentence into a list and each word is going to be each element of a list """
+print(len(split_list))
